e1eef89c5f2c9ba2f7203a0aadecebdcb4547fa7
saadrehman171000 <saadrehman1710000@gmail.com>
2025-07-29 15:24:24 +0500

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,14 +19,20 @@ export default function RootLayout({
         <meta name="msapplication-TileColor" content="#003e94" />
         <meta name="theme-color" content="#003e94" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+        
+        {/* Favicon Configuration */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        
+        {/* Preconnect for performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico?v=8" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=8" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=8" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=8" />
-        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={inter.className}>
         <ClerkProvider 
